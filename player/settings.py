9ca1efc21ae853f672ab9ed3ed31f963e68605eb
narendrayad <narendrayadav.anshu@gmail.com>
2022-09-15 14:57:56 +0530

    commit

--- player/settings.py
+++ player/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL= "/media/"
 MEDIA_ROOT= os.path.join(BASE_DIR,"media/")
 
